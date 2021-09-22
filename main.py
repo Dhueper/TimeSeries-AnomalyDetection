2364e67e7ae93611209a1fe213a963fc929fb64e
David <davihup@gmail.com>
2021-09-22 11:08:44 +0200

    Time series decomposition

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+from dateutil.parser import parse
+from statsmodels.tsa.seasonal import seasonal_decompose
+from matplotlib import pyplot as plt
 
 import test_function
 
@@ -7,8 +10,35 @@ import test_function
 
 [t, X] = test_function.solar_power_sso() 
 
-time_serie = np.transpose(np.array([t,X]))
+time_series = np.transpose(np.array([t,X]))
 
-df = pd.DataFrame(time_serie, columns=["time", "X(t)"])
+
+df = pd.DataFrame(time_series, columns=["time", "X(t)"]) 
+
+df.set_index("time", inplace=True)
 
 print(df)
+
+plt.figure()
+plt.plot(t,X)
+plt.xlabel('t')
+plt.ylabel('X(t)')
+plt.title('Original time series') 
+plt.show()
+
+decomposition = seasonal_decompose(df['X(t)'], model="additive", period=5000)
+
+plt.figure()
+plt.subplot(3,1,1)
+plt.plot(t,decomposition.trend)
+plt.xlabel('t')
+plt.title('Trend component') 
+plt.subplot(3,1,2)
+plt.plot(t,decomposition.seasonal)
+plt.xlabel('t')
+plt.title('Seasonal component') 
+plt.subplot(3,1,3)
+plt.plot(t,decomposition.resid)
+plt.xlabel('t')
+plt.title('Irregular variations') 
+plt.show()
