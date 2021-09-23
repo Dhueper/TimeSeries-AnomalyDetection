9d4f87f16b10d3d73cc012deb8657462739cb118
David <davihup@gmail.com>
2021-09-23 14:00:26 +0200

    warning messages

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 import numpy as np
 import pandas as pd
-from dateutil.parser import parse
-from statsmodels.tsa.seasonal import seasonal_decompose, STL
 from matplotlib import pyplot as plt
-from scipy.fft import fft, fftfreq
 
 import test_function
+import ts_analysis
 
 #%% Time series definition
-
 [t, X] = test_function.solar_power_sso(1) 
 
 time_series = np.transpose(np.array([t,X]))
 
-
 df = pd.DataFrame(time_series, columns=["time", "X(t)"]) 
-
 df.set_index("time", inplace=True)
-
-print(df)
 
 plt.figure()
 plt.plot(t,X)
@@ -27,46 +20,5 @@ plt.ylabel('X(t)')
 plt.title('Original time series') 
 plt.show()
 
-#%% FFT
-yf = fft(X)[0:len(X)//2] 
-xf = fftfreq(len(t),t[1] - t[0])[:len(X)//2] 
+decomposition = ts_analysis.ts_decomposition(df, plot=False)
 
-yf_max = np.max(yf)
-yf_order = np.flip(np.argsort(np.abs(yf)))
-threshold = 0.05
-
-for i in yf_order:
-    if abs(yf[i]) < threshold*yf_max:
-        xf_th = xf[i]  
-        break
-
-plt.figure()
-plt.plot(xf, 2/len(X) * np.abs(yf))
-plt.xlabel('f [Hz]')
-plt.ylabel('FFT')
-plt.title('FFT time series') 
-plt.show()
-
-#%% Time series decomposition
-period = int(2*np.pi/(0.05*(t[1] - t[0])))
-periodf = int(1./(xf_th*(t[1] - t[0])))
-print(period/periodf)
-
-decomposition = seasonal_decompose(df['X(t)'], model="additive", period=periodf)
-
-plt.figure()
-plt.subplot(3,1,1)
-plt.plot(t,decomposition.trend)
-plt.xlabel('t')
-plt.title('Trend component') 
-plt.subplot(3,1,2)
-plt.plot(t,decomposition.seasonal)
-plt.xlabel('t')
-plt.title('Seasonal component') 
-plt.subplot(3,1,3)
-plt.plot(t,decomposition.resid)
-plt.xlabel('t')
-plt.title('Irregular variations') 
-plt.show()
-
-# %%
---
+++ ts_analysis.py
@@ -0,0 +1,82 @@
+import warnings
+import numpy as np
+from statsmodels.tsa.seasonal import seasonal_decompose
+from scipy.fft import fft, fftfreq
+from matplotlib import pyplot as plt
+
+def ts_decomposition(df,**kwargs):
+    """Time series decomposition in trend, seasonal/cyclic variations
+    and irregular variations.
+
+        Intent(in): df(pandas DataFrame), time series;
+        kwargs (optional): period(integer), time series period;
+        plot(boolean), plot results, default = True
+
+        Returns: decomposition(statsmodels class), time series decomposed in .trend, .seasonal and .resid
+    """
+
+    if "plot" in kwargs:
+        plot = kwargs["plot"] 
+        if type(plot) != bool:
+            plot = True
+            warnings.warn("plot argument must be of type bool, it has been set True by default.", stacklevel=2)
+    else:
+        plot = True
+
+    X = np.array(df["X(t)"])
+    t = np.array(df.index)
+
+    # Analysis in frequency domain: FFT
+    yf = fft(X)
+    xf = fftfreq(len(t),t[1]-t[0])
+
+    yf_max = np.max(yf)
+    yf_order = np.flip(np.argsort(np.abs(yf[0:len(X)//2])))
+    threshold = 0.05
+
+    for i in yf_order:
+        if abs(yf[i]) < threshold*yf_max:
+            xf_th = xf[i]  
+            break
+
+    if plot:
+        plt.figure()
+        plt.plot(xf[:len(X)//2], 2/len(X) * np.abs(yf[0:len(X)//2]))
+        plt.xlabel('f [Hz]')
+        plt.ylabel('FFT')
+        plt.title('FFT time series') 
+        plt.show()
+
+    # Time series decomposition
+    if "period" in kwargs:
+        try:
+            if type(kwargs["period"]) == int:
+                period = kwargs["period"]
+            else:
+                period = int(kwargs["period"])
+                warnings.warn("period argument must be of type integer, it has been rounded by default.", stacklevel=2)
+        except:
+            warnings.warn("period argument must be of type integer, it has been automatically computed.", stacklevel=2)
+            period = int(1./(xf_th*(t[1] - t[0])))
+    else:
+        period = int(1./(xf_th*(t[1] - t[0])))
+
+    decomposition = seasonal_decompose(df['X(t)'], model="additive", period=period)
+
+    if plot:
+        plt.figure()
+        plt.subplot(3,1,1)
+        plt.plot(t,decomposition.trend)
+        plt.xlabel('t')
+        plt.title('Trend component') 
+        plt.subplot(3,1,2)
+        plt.plot(t,decomposition.seasonal)
+        plt.xlabel('t')
+        plt.title('Seasonal component') 
+        plt.subplot(3,1,3)
+        plt.plot(t,decomposition.resid)
+        plt.xlabel('t')
+        plt.title('Irregular variations') 
+        plt.show()
+
+    return decomposition
