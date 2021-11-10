2730b427abdc31d82170ef4240ece869cda25ebc
David <davihup@gmail.com>
2021-11-10 11:05:49 +0100

    Effective gain plots

--- plots.py
+++ plots.py
@@ -13,12 +13,27 @@ if __name__ == "__main__":
     legend =[] 
     plt.plot(t, 0*t, 'r--')
     legend.append('Maximum dampening')
-    for i in range(-1,4):
+    for i in range(0,4):
         plt.plot(t, gain_periodic(t,i/2.)) 
         legend.append(r'$\alpha$='+str(i/2.))
     plt.xlabel('$\it{f}$', fontsize = 18)
     plt.ylabel('$\it{G}$', fontsize = 18, rotation=0)
     plt.legend(legend, fontsize = 18)
+    plt.xticks(fontsize = 18)
+    plt.yticks(fontsize = 18)
+    plt.show()
+
+    G0 = gain_periodic(t,1)
+    plt.figure()
+    legend =[] 
+    plt.plot(t, 0*t, 'r--')
+    legend.append('Maximum dampening')
+    for i in range(1,6):
+        plt.plot(t, G0**i) 
+        legend.append('$\it{N}$='+str(i))
+    plt.xlabel('$\it{f}$', fontsize = 18)
+    plt.ylabel('$\it{G^N}$', fontsize = 18, rotation=0)
+    plt.legend(legend, fontsize = 18, loc = 'lower left')
     plt.xticks(fontsize = 18)
     plt.yticks(fontsize = 18)
     plt.show()
